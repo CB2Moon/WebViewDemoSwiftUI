9d1929506af1a3b2e285786006f01bd99e02875a
CB2Moon <hideMYmask@outlook.com>
2024-07-14 00:23:02 +1000

    Initial Commit

---
+++ WebViewDemoSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WebViewDemoSwiftUI
+//
+//  Created by CB on 14/7/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WebViewDemoSwiftUI/WebViewDemoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewDemoSwiftUIApp.swift
+//  WebViewDemoSwiftUI
+//
+//  Created by CB on 14/7/2024.
+//
+
+import SwiftUI
+
+@main
+struct WebViewDemoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
